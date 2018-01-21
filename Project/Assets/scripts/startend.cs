1c434367e91ea78f17f5e892bd52112d8e7f60bf
Savran Taha <savrantaha@hotmail.com>
2018-01-21 20:59:52 +0100

    Added return to menu, (BETA)
    Groepsleden test deze versie aub

---
+++ Project/Assets/scripts/startend.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startend : MonoBehaviour
+{
+
+    private string pos_tag_name = "Finish";
+
+    public GameObject rayPoint;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && rayPoint != null)
+        {
+            ButtonPressed();
+        }
+    }
+    void ButtonPressed()
+    {
+        RaycastHit Hit = new RaycastHit();
+
+        if (Physics.Raycast(rayPoint.transform.position, rayPoint.transform.forward, out Hit, 250))
+        {
+            Debug.Log("ray");
+            Debug.DrawRay(rayPoint.transform.position, rayPoint.transform.forward * 250, Color.yellow);
+
+            if (Hit.transform.gameObject.tag == pos_tag_name)
+            {
+                Application.LoadLevel(0);
+            }
+        }
+    }
+}
